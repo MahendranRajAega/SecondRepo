356ed03a73cb8e5e28ecaf9dbfc396459b40c244
MahendranRajAega <saiaega17@gmail.com>
2025-08-29 00:14:35 +0530

    added run bat file

---
+++ TestingReoi/run_env.bat
@@ -0,0 +1,11 @@
+@echo off
+REM $1 = environment parameter
+SET ENV=%1
+
+REM Set port and log file dynamically
+IF "%ENV%"=="dev" SET PORT=1990
+IF "%ENV%"=="sit" SET PORT=1991
+IF "%ENV%"=="prod" SET PORT=1992
+
+REM Start Spring Boot in detached mode
+start "" java -jar target/myapp.jar --spring.profiles.active=%ENV% > app-%ENV%.log 2>&1
